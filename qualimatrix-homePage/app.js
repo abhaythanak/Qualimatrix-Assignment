39b007215a995c4a1a52ddf79996d347d7769c77
Abhay Thanak <abhaythanak@gmail.com>
2023-11-18 15:48:05 +0530

    final commit technology components part is not complete but almost done i tried to add custom cursor but some error happen i will try to fixed it

--- qualimatrix-homePage/app.js
+++ qualimatrix-homePage/app.js
@@ -1,4 +1,13 @@
-  // JavaScript code to loop the typewriter effect
+// custom cursor
+let main = document.querySelector(".main")
+let cursor = document.querySelector(".cursor")
+
+main.addEventListener("mousemove", function(e){
+    cursor.style.left = e.x+"px"
+    cursor.style.top = e.y+"px"
+})
+
+// JavaScript code to loop the typewriter effect
   const textElement = document.querySelector('.text');
 
   function resetAnimation() {
